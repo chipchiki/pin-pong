1c3a5e8a344dcdfc6bb05a763ac3944b0a8a3abd
chipchiki <k-kireev10@yandex.ru>
2025-04-10 18:04:27 +0400

    обнова_2

--- pin_pong.py
+++ pin_pong.py
@@ -13,12 +13,18 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
 class Player(GameSprite):
-    def update(self):
+    def update_left(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x >5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < 700 - 80:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y >5:
+            self.rect.y-= self.speed
+        if keys[K_s] and self.rect.y < height - 80:
+            self.rect.y += self.speed
+    def update_right(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y >5:
+            self.rect.y-= self.speed
+        if keys[K_DOWN] and self.rect.y < height - 80:
+            self.rect.y += self.speed
     def fire(self):
         bullet = Bullet('bullet.png', self.rect.centerx, self.rect.centery,5)
         bullets.add(bullet)
@@ -32,12 +38,22 @@ window = display.set_mode((width,height))
 window.fill((0,0,0))
 clock = time.Clock()
 FPS = 60
-
+player_one = Player('43.jpg', 10, 10, 10)
+player_two = Player('1.jpg', width-80, 10, 10)
 start = True
 
 while start == True:
+
+
+    player_one.update_right()
+    player_one.reset()
+
+    player_two.update_left()
+    player_two.reset()
+
     for e in event.get():
         if e.type == QUIT:
             start = False
+    
     display.update()
     clock.tick(FPS)
